1143d8dad591ef0f0b9aac9af3a96c05552444cf
Rakesh V <147958244+Rakesh7890v@users.noreply.github.com>
2025-05-05 20:59:55 +0530

    Update adminlogin.js

--- static/adminlogin.js
+++ static/adminlogin.js
@@ -4,7 +4,7 @@ document.getElementById("form").addEventListener("submit", function(e) {
     const errorMsg = document.getElementById("error-msg");
 
     if (password === "abcd") {
-        window.location.href = "admin.html";
+        window.location.href = "./admin.html";
     } else {
         errorMsg.textContent = "Invalid password. Please try again.";
     }
